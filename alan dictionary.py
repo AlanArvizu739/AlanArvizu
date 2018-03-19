a1752403d474cafc9b40205a44009ed1ed01ae03
Alan Raul Arvizu <alan.arvizu@stu.jefferson.kyschools.us>
2018-03-19 08:15:12 -0400

    commit

---
+++ alan dictionary.py
@@ -0,0 +1,13 @@
+#Alan Arvizu March 16 , 2018
+
+Cristiano_Ronaldo = {
+    "Position": "forward",
+    "Height": "6'0 (1.84m)",
+    "Weight": "174 lbs (79kg)",
+    "Age": "33",
+    "DOB": "February 5, 1985",
+    "Team": "Real Madrid",
+}
+
+for key in Cristiano_Ronaldo:
+    print "key: " + str(key)
